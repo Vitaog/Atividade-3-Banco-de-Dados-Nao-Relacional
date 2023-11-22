abbf9f74ea873467da7fe2b5e6955cffb304daea
Vitor Garcez <vitaoog@gmail.com>
2023-11-22 00:53:44 -0300

    Adição edição e delete vendedor

--- src/menu.py
+++ src/menu.py
@@ -1,6 +1,6 @@
 # Defina a URI do seu banco de dados MongoDB aqui
 from produto import atualizar_produto_mongodb_e_redis, deletar_produto_redis, exportar_produtos_mongodb_para_redis, imprimir_produtos_redis
-from vendedor import exportar_vendedores_mongodb_para_redis, imprimir_vendedores_redis 
+from vendedor import deletar_vendedor_redis, editar_vendedor, exportar_vendedores_mongodb_para_redis, imprimir_vendedores_redis 
 
 # Configuração do MongoDB
 MONGO_URI = ""
@@ -55,5 +55,9 @@ while (key != 'S' and key != 's'):
             exportar_vendedores_mongodb_para_redis(MONGO_URI,REDIS_HOST,REDIS_PORT,REDIS_PASSWORD)
         elif (sub == '2'):
             imprimir_vendedores_redis(REDIS_HOST,REDIS_PORT,REDIS_PASSWORD)
+        elif (sub == '3'):
+            editar_vendedor(MONGO_URI,REDIS_HOST,REDIS_PORT,REDIS_PASSWORD)
+        elif (sub == '4'):
+            deletar_vendedor_redis(REDIS_HOST,REDIS_PORT,REDIS_PASSWORD)
   
 print("Vlw Flw...")
--- src/vendedor.py
+++ src/vendedor.py
@@ -79,3 +79,165 @@ def imprimir_vendedores_redis(redis_host, redis_port, redis_password):
 
     # Fechar a conexão
     cliente_redis.close()
+
+def editar_vendedor(mongo_uri, redis_host, redis_port, redis_password):
+    # Conectar ao Redis
+    cliente_redis = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+
+    # Conectar ao MongoDB
+    cliente_mongo = MongoClient(mongo_uri)
+    banco_de_dados_mongo = cliente_mongo.get_database("MercadoLivreRedis")
+    colecao_vendedor_mongo = banco_de_dados_mongo.get_collection("Vendedor")
+
+    # Obter todas as chaves no Redis (cada chave representa um vendedor)
+    chaves_vendedores = cliente_redis.keys("Vendedor:*")
+
+    if not chaves_vendedores:
+        print("Nenhum vendedor cadastrado.")
+    else:
+        # Mostrar os nomes dos vendedores e obter a seleção do usuário
+        print("Selecione um vendedor para editar:")
+        for i, chave_vendedor in enumerate(chaves_vendedores, start=1):
+            nome_vendedor = cliente_redis.hget(chave_vendedor, 'nome_vendedor')
+            print(f"{i}. {nome_vendedor}")
+
+        # Pedir ao usuário para selecionar um vendedor pelo índice
+        try:
+            indice_selecionado = int(input("Digite o número correspondente ao vendedor que deseja editar: "))
+        except ValueError:
+            print("Entrada inválida. Digite um número válido.")
+            return
+
+        # Verificar se o índice está dentro dos limites
+        if 1 <= indice_selecionado <= len(chaves_vendedores):
+            # Obter a chave correspondente ao índice selecionado
+            chave_vendedor_selecionado = chaves_vendedores[indice_selecionado - 1]
+            dados_vendedor = cliente_redis.hgetall(chave_vendedor_selecionado)
+
+            # Mostrar os dados do vendedor selecionado
+            print(f"\nNome do vendedor: {dados_vendedor['nome_vendedor']}")
+            print(f"Data de cadastro: {dados_vendedor['data_cadastro']}")
+
+            # Opção de editar o nome do vendedor
+            novo_nome_vendedor = input(f"Digite o novo nome para o vendedor '{dados_vendedor['nome_vendedor']}' (ou pressione Enter para manter): ")
+
+            # Verificar se o usuário deseja manter o valor existente
+            if not novo_nome_vendedor:
+                novo_nome_vendedor = dados_vendedor['nome_vendedor']
+
+            # Atualizar a chave do vendedor no Redis
+            nova_chave_redis = f"Vendedor:{novo_nome_vendedor}"
+            cliente_redis.rename(chave_vendedor_selecionado, nova_chave_redis)
+
+            # Atualizar o nome do vendedor no Redis
+            cliente_redis.hset(nova_chave_redis, 'nome_vendedor', novo_nome_vendedor)
+
+            # Atualizar o nome do vendedor nos produtos associados
+            produtos = json.loads(dados_vendedor['produtos'])
+            for produto in produtos:
+                produto['nome_vendedor'] = novo_nome_vendedor
+
+            # Atualizar a string JSON de produtos no Redis
+            cliente_redis.hset(nova_chave_redis, 'produtos', json.dumps(produtos))
+
+            # Atualizar o nome do vendedor no MongoDB
+            filtro = {'nome_vendedor': dados_vendedor['nome_vendedor']}
+            atualizacao = {'$set': {'nome_vendedor': novo_nome_vendedor}}
+            colecao_vendedor_mongo.update_one(filtro, atualizacao)
+
+            # Opção de selecionar um produto para editar
+            print("\nProdutos do vendedor:")
+            for j, produto in enumerate(produtos, start=1):
+                print(f"{j}. {produto['nome_produto']}  Preço: {produto['preco']}  Qtd: {produto['quantidade_disponivel']}")
+
+            try:
+                indice_produto_selecionado = int(input("Digite o número correspondente ao produto que deseja editar: "))
+            except ValueError:
+                print("Entrada inválida. Digite um número válido.")
+                return
+
+            # Verificar se o índice está dentro dos limites
+            if 1 <= indice_produto_selecionado <= len(produtos):
+                # Obter o produto correspondente ao índice selecionado
+                produto_selecionado = produtos[indice_produto_selecionado - 1]
+
+                # Solicitar a atualização do preço e da quantidade disponível
+                novo_preco = input(f"Digite o novo preço para o produto '{produto_selecionado['nome_produto']}' (ou pressione Enter para manter): ")
+                nova_quantidade = input(f"Digite a nova quantidade disponível para o produto '{produto_selecionado['nome_produto']}' (ou pressione Enter para manter): ")
+
+                # Verificar se o usuário deseja manter os valores existentes
+                if not novo_preco:
+                    novo_preco = produto_selecionado['preco']
+                if not nova_quantidade:
+                    nova_quantidade = produto_selecionado['quantidade_disponivel']
+
+                # Atualizar o produto no Redis
+                produtos[indice_produto_selecionado - 1]['preco'] = novo_preco
+                produtos[indice_produto_selecionado - 1]['quantidade_disponivel'] = nova_quantidade
+
+                # Atualizar a string JSON de produtos no Redis
+                cliente_redis.hset(nova_chave_redis, 'produtos', json.dumps(produtos))
+
+                # Atualizar o produto no MongoDB
+                filtro_produto = {
+                    'nome_vendedor': novo_nome_vendedor,
+                    'produtos': {'$elemMatch': {'nome_produto': produto_selecionado['nome_produto']}}
+                }
+                atualizacao_produto = {
+                    '$set': {
+                        'produtos.$.preco': novo_preco,
+                        'produtos.$.quantidade_disponivel': nova_quantidade
+                    }
+                }
+                colecao_vendedor_mongo.update_one(filtro_produto, atualizacao_produto)
+
+                print(f"Vendedor '{novo_nome_vendedor}', produto '{produto_selecionado['nome_produto']}' atualizado com sucesso.")
+            else:
+                print("Índice inválido. Selecione um número válido.")
+        else:
+            print("Índice inválido. Selecione um número válido.")
+
+    # Fechar as conexões
+    cliente_redis.close()
+    cliente_mongo.close()
+
+def deletar_vendedor_redis(redis_host, redis_port, redis_password):
+    # Conectar ao Redis
+    cliente_redis = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+
+    # Obter todas as chaves no Redis (cada chave representa um vendedor)
+    chaves_vendedores = cliente_redis.keys("Vendedor:*")
+
+    if not chaves_vendedores:
+        print("Nenhum vendedor cadastrado.")
+    else:
+        # Mostrar os nomes dos vendedores e obter a seleção do usuário
+        print("Selecione um vendedor para deletar:")
+        for i, chave_vendedor in enumerate(chaves_vendedores, start=1):
+            nome_vendedor = cliente_redis.hget(chave_vendedor, 'nome_vendedor')
+            print(f"{i}. {nome_vendedor}")
+
+        # Pedir ao usuário para selecionar um vendedor pelo índice
+        try:
+            indice_selecionado = int(input("Digite o número correspondente ao vendedor que deseja deletar: "))
+        except ValueError:
+            print("Entrada inválida. Digite um número válido.")
+            return
+
+        # Verificar se o índice está dentro dos limites
+        if 1 <= indice_selecionado <= len(chaves_vendedores):
+            # Obter a chave correspondente ao índice selecionado
+            chave_vendedor_selecionado = chaves_vendedores[indice_selecionado - 1]
+
+            # Obter o nome do vendedor
+            nome_vendedor_selecionado = cliente_redis.hget(chave_vendedor_selecionado, 'nome_vendedor')
+
+            # Deletar o vendedor no Redis
+            cliente_redis.delete(chave_vendedor_selecionado)
+
+            print(f"Vendedor '{nome_vendedor_selecionado}' deletado com sucesso do Redis.")
+        else:
+            print("Índice inválido. Selecione um número válido.")
+
+    # Fechar a conexão
+    cliente_redis.close()
